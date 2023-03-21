a819bf6ce03c7715167ae4e442f75b510227417b
Minoru Nakano <minoru.nakano@gmail.com>
2023-03-21 13:00:49 -0400

    testing

--- scripts/get_Cleveland.R
+++ scripts/get_Cleveland.R
@@ -6,10 +6,10 @@
 
 # library(Biobase)
 # library(xlsx)
-library(gdata)
-library(parallel)
-library(abind)
-library(RadioGx)
+# library(gdata)
+# library(parallel)
+# library(abind)
+# library(RadioGx)
 library(PharmacoGx)
 options(stringsAsFactors = FALSE)
 
@@ -17,147 +17,88 @@ args <- commandArgs(trailingOnly = TRUE)
 work_dir <- args[1]
 filename <- args[2]
 
-responsedata <- read.xls(file.path(work_dir, "download/XRT_CTD2_Dose_Response.xlsx"), 1, stringsAsFactors = F)
-responsedata <- responsedata[, -c(43:48)]
-# responsedata$cell_line <- gsub("786O", "7860", responsedata$cell_line)
-# responsedata$cell_line <- gsub("COLO320", "COLO320HSR", responsedata$cell_line)
-# responsedata$cell_line <- gsub("COLO320", "COLO320HSR", responsedata$cell_line)
-# responsedata$cell_line <- gsub("786O", "7860", responsedata$cell_line)
+# responsedata <- read.xls(file.path(work_dir, "download/XRT_CTD2_Dose_Response.xlsx"), 1, stringsAsFactors = F)
+# responsedata <- responsedata[, -c(43:48)]
 
 
-#### This is the code to do the matching for when this dataset was first annotated.
+# #### Now the cell.all file has the correct annotations.
 
-# cell.all <- read.csv(system.file("extdata", "cell_annotation_all.csv",package="PharmacoGxPrivate"))
+# cell.all <- read.csv(file.path(work_dir, "download/cell_annotation_all.csv"))
+# # cell.all <- read.csv("~/Code/Github/PharmacoGx-private/inst/extdata/cell_annotation_all.csv")
+# cell.exact.match <- match(responsedata$cell_line, cell.all$Cleveland.cellid)
 
-# cell.exact.match <- match(gsub("\\s", "", x = toupper(responsedata$cell_line)), gsub("\\s", "", x = toupper(cell.all[,"CTRPv2.cellid"])))
-# cell.exact.match.ccle <- match(gsub("\\s", "", x = toupper(responsedata$cell_line)), gsub("\\s", "", x = toupper(cell.all[,"CCLE.cellid"])))
-# # cell.exact.match.ccle_rnaseq <- match(gsub("\\s", "", x = toupper(responsedata$cell_line)), gsub("\\s", "", x = toupper(cell.all[,"CCLE_rnaseq.cellid"])))
-# ### matching to unique cellid didnt help this time.
+# unique.cellid <- cell.all[cell.exact.match, "unique.cellid"]
+# unique.tissueid <- cell.all[cell.exact.match, "unique.tissueid"]
+# # rownames(responsedata) <- responsedata$cell_line
 
-# no.matches <- is.na(cell.exact.match) & is.na(cell.exact.match.ccle)
-# # no.matches <- responsedata$cell_line[which(is.na(cell.exact.match) & is.na(cell.exact.match.ccle) & is.na(cell.exact.match.ccle_rnaseq))]
-
-# # no.matches2 <- gsub(x=no.matches, "NCI", "")
-# # no.matches2 <- gsub(x=no.matches2, "NIH", "")
-
-
-# approx.matches <- sapply(responsedata$cell_line[which(no.matches)],function(x) {
-# 	paste(cell.all[agrep(gsub("\\s", "", x = toupper(x)),
-# 		  gsub("\\s", "", x = toupper(cell.all[,"unique.cellid"]))),"unique.cellid"], collapse = "|||")}
+# responsedata1 <- responsedata[, c(2:12, 22, 25, 40:42)]
+# colnames(responsedata1) <- c(
+#   "CellLine", "SeedingDensity", "1Gy-rep1", "1Gy-rep2", "2Gy", "3Gy", "4Gy", "5Gy", "6Gy", "8Gy", "10Gy", "AUC", "CellLineSynonym",
+#   "Primarysite", "Histology", "HistologySubType"
 # )
 
-# write.csv(cbind(responsedata$cell_line[which(no.matches)], approx.matches), file="cleveland_approx_matches.csv")
+# cell <- data.frame(responsedata1$CellLine, responsedata1$Primarysite, responsedata1$Histology, responsedata1$HistologySubType)
+# cell <- cbind(cellid = unique.cellid, tissueid = unique.tissueid, cell)
 
-# after.matching <- read.csv("cleveland_approx_matches.csv")
+# cell <- cell[!duplicated(cell$cellid), ]
 
-# cell.exact.match <- sapply(seq_along(cell.exact.match), function(i){
-# 	if(is.na(cell.exact.match[[i]])){
-# 		return(cell.exact.match.ccle[[i]])
-# 	}
-# 	else {
-# 		return(cell.exact.match[[i]])
-# 	}
-# })
+# colnames(cell) <- c("cellid", "tissueid", "CellLine", "Primarysite", "Histology", "Subhistology")
+# rownames(cell) <- cell$cellid
 
-# cell.match.table <- data.frame(Cleveland.cellid = responsedata$cell_line,
-# 							   unique.cellid = cell.all[cell.exact.match,"unique.cellid"])
+# # sensitivity object
+# info <- data.frame(
+#   unique.cellid, rep("radiation", nrow(responsedata1)), rep(1, each = nrow(responsedata1)), rep(1, each = nrow(responsedata1)),
+#   rep(2, each = nrow(responsedata1)), rep(3, each = nrow(responsedata1)), rep(4, each = nrow(responsedata1)), rep(5, each = nrow(responsedata1)),
+#   rep(6, each = nrow(responsedata1)), rep(8, each = nrow(responsedata1)), rep(10, each = nrow(responsedata1))
+# )
+# colnames(info) <- c(
+#   "cellid", "radiation.type", "Dose1-1Gy-rep1", "Dose1-1Gy-rep2", "Dose2-2Gy", "Dose3-3Gy", "Dose4-4Gy",
+#   "Dose5-5Gy", "Dose6-6Gy", "Dose8-8Gy", "Dose10-10Gy"
+# )
+# rownames(info) <- paste(info$cellid, info$radiationtype, seq(1, nrow(info)), sep = "_")
 
-# cell.match.table[no.matches,"unique.cellid"] <- after.matching$actual.matches
-# cell.match.table[no.matches,]
+# test1 <- info[, c(3:11)]
+# test2 <- responsedata1[, c(3:11)]
+# raw <- abind(test1, test2, along = 3)
+# rownames(raw) <- rownames(info)
 
-# cell.all <- cbind(cell.all, "Cleveland.cellid" = NA_character_, "Cleveland.tissueid" = NA_character_)
+# colnames(raw) <- paste0("doses", seq_len(ncol(raw)))
 
-# myx <- match(cell.match.table[,"unique.cellid"], cell.all[,"unique.cellid"])
+# dimnames(raw)[[3]] <- c("Dose", "Viability")
 
-# cell.all[na.omit(myx),"Cleveland.cellid"] <- cell.match.table[!is.na(myx), "Cleveland.cellid"]
-# cell.all[na.omit(myx),"Cleveland.tissueid"] <- responsedata[!is.na(myx), 40]
+# profiles <- data.frame(responsedata1$AUC)
+# colnames(profiles) <- c("AUC_published")
+# rownames(profiles) <- rownames(info)
 
-# for(i in seq_len(sum(is.na(myx)))){
-# 	cell.all <- rbind(cell.all, rep(NA,ncol(cell.all)))
-# 	cell.all[nrow(cell.all),c("Cleveland.cellid","unique.cellid")] <- cell.match.table[which(is.na(myx))[i],]
-# 	cell.all[nrow(cell.all),"Cleveland.tissueid"] <- responsedata[which(is.na(myx))[i],40]
-# }
-# write.csv(cell.all, file="~/Code/Github/PharmacoGx-private/inst/extdata/cell_annotation_all.csv")
+# CCLE <- readRDS(file.path(work_dir, "download/CCLE.rds"))
 
+# curationCell <- cell.all[cell.exact.match, c("unique.cellid", "Cleveland.cellid")]
+# curationTissue <- cell.all[cell.exact.match, c("unique.tissueid", "Cleveland.tissueid")]
 
-#### Now the cell.all file has the correct annotations.
+# curationTissue <- curationTissue[!duplicated(curationCell[, "unique.cellid"]), ]
+# curationCell <- curationCell[!duplicated(curationCell[, "unique.cellid"]), ]
+# rownames(curationCell) <- curationCell[, "unique.cellid"]
+# rownames(curationTissue) <- curationCell[, "unique.cellid"]
 
-cell.all <- read.csv(file.path(work_dir, "download/cell_annotation_all.csv"))
-# cell.all <- read.csv("~/Code/Github/PharmacoGx-private/inst/extdata/cell_annotation_all.csv")
-cell.exact.match <- match(responsedata$cell_line, cell.all$Cleveland.cellid)
-
-unique.cellid <- cell.all[cell.exact.match, "unique.cellid"]
-unique.tissueid <- cell.all[cell.exact.match, "unique.tissueid"]
-# rownames(responsedata) <- responsedata$cell_line
-
-responsedata1 <- responsedata[, c(2:12, 22, 25, 40:42)]
-colnames(responsedata1) <- c(
-  "CellLine", "SeedingDensity", "1Gy-rep1", "1Gy-rep2", "2Gy", "3Gy", "4Gy", "5Gy", "6Gy", "8Gy", "10Gy", "AUC", "CellLineSynonym",
-  "Primarysite", "Histology", "HistologySubType"
-)
-
-cell <- data.frame(responsedata1$CellLine, responsedata1$Primarysite, responsedata1$Histology, responsedata1$HistologySubType)
-cell <- cbind(cellid = unique.cellid, tissueid = unique.tissueid, cell)
-
-cell <- cell[!duplicated(cell$cellid), ]
-
-colnames(cell) <- c("cellid", "tissueid", "CellLine", "Primarysite", "Histology", "Subhistology")
-rownames(cell) <- cell$cellid
-
-# sensitivity object
-info <- data.frame(
-  unique.cellid, rep("radiation", nrow(responsedata1)), rep(1, each = nrow(responsedata1)), rep(1, each = nrow(responsedata1)),
-  rep(2, each = nrow(responsedata1)), rep(3, each = nrow(responsedata1)), rep(4, each = nrow(responsedata1)), rep(5, each = nrow(responsedata1)),
-  rep(6, each = nrow(responsedata1)), rep(8, each = nrow(responsedata1)), rep(10, each = nrow(responsedata1))
-)
-colnames(info) <- c(
-  "cellid", "radiation.type", "Dose1-1Gy-rep1", "Dose1-1Gy-rep2", "Dose2-2Gy", "Dose3-3Gy", "Dose4-4Gy",
-  "Dose5-5Gy", "Dose6-6Gy", "Dose8-8Gy", "Dose10-10Gy"
-)
-rownames(info) <- paste(info$cellid, info$radiationtype, seq(1, nrow(info)), sep = "_")
-
-test1 <- info[, c(3:11)]
-test2 <- responsedata1[, c(3:11)]
-raw <- abind(test1, test2, along = 3)
-rownames(raw) <- rownames(info)
-
-colnames(raw) <- paste0("doses", seq_len(ncol(raw)))
-
-dimnames(raw)[[3]] <- c("Dose", "Viability")
-
-profiles <- data.frame(responsedata1$AUC)
-colnames(profiles) <- c("AUC_published")
-rownames(profiles) <- rownames(info)
-
-CCLE <- readRDS(file.path(work_dir, "download/CCLE.rds"))
-
-curationCell <- cell.all[cell.exact.match, c("unique.cellid", "Cleveland.cellid")]
-curationTissue <- cell.all[cell.exact.match, c("unique.tissueid", "Cleveland.tissueid")]
-
-curationTissue <- curationTissue[!duplicated(curationCell[, "unique.cellid"]), ]
-curationCell <- curationCell[!duplicated(curationCell[, "unique.cellid"]), ]
-rownames(curationCell) <- curationCell[, "unique.cellid"]
-rownames(curationTissue) <- curationCell[, "unique.cellid"]
-
-cells <- PharmacoGx::cellInfo(CCLE)
-CCLEint <- PharmacoGx::subsetTo(CCLE, cells = intersect(rownames(cell), rownames(cells)))
+# cells <- PharmacoGx::cellInfo(CCLE)
+# CCLEint <- PharmacoGx::subsetTo(CCLE, cells = intersect(rownames(cell), rownames(cells)))
 
 
-Cleveland <- RadioSet(
-  name = "Cleveland",
-  cell = cell,
-  molecularProfiles = CCLEint@molecularProfiles,
-  radiation = data.frame("radiation", row.names = "radiation"),
-  sensitivityInfo = info,
-  sensitivityRaw = raw,
-  sensitivityProfiles = profiles,
-  datasetType = c("sensitivity"),
-  curationCell = curationCell,
-  curationTissue = curationTissue,
-  verify = TRUE
-)
+# Cleveland <- RadioSet(
+#   name = "Cleveland",
+#   cell = cell,
+#   molecularProfiles = CCLEint@molecularProfiles,
+#   radiation = data.frame("radiation", row.names = "radiation"),
+#   sensitivityInfo = info,
+#   sensitivityRaw = raw,
+#   sensitivityProfiles = profiles,
+#   datasetType = c("sensitivity"),
+#   curationCell = curationCell,
+#   curationTissue = curationTissue,
+#   verify = TRUE
+# )
 
-saveRDS(Cleveland, file = file.path(work_dir, filename))
+# saveRDS(Cleveland, file = file.path(work_dir, filename))
 
 
 # radSig <- radSensitivitySig(Cleveland, "Kallisto_0.46.1.rnaseq", sensitivity.measure="AUC_published", nthread=3)
